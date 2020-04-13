8f6ac492e0077a66be18e85c5b41b17e17c6e898
Javahzade <cavid.haxizade@gmail.com>
2020-04-13 14:26:14 +0400

    Complite Calculator

--- App.js
+++ App.js
@@ -8,35 +8,75 @@ export default class App extends Component {
     this.state = {
       resultText: '',
       calculationText: '',
+      lastText: '',
     };
   }
 
-  buttonsPressed(text) {
-    console.log(text);
+  numberPressed(number) {
     this.setState({
-      resultText: this.state.resultText + text,
-    });
+      resultText: this.state.resultText + number,
+      lastText: this.state.lastText + number,
+    });  
+  }
+ 
+  operatorPressed(operand) {
+    if (this.state.resultText === ''){
+      this.setState({
+        resultText: '',
+        lastText: '',
+      });
+    }else {
+      if (this.state.resultText[this.state.resultText.length-1] === operand ){
+        this.setState({
+          resultText: this.state.resultText,
+          lastText: '',
+        });
+      }else {
+        this.setState({
+          resultText: this.state.resultText + operand,
+          lastText: '',
+        });
+      }
+      
+    }
+  }
+  dotPressed(dot) {
+    if (!this.state.lastText.includes(dot)){
+      this.setState({
+        resultText: this.state.resultText + dot,
+        lastText: this.state.lastText + dot,
+      });
+    }
   }
 
   clear() {
     this.setState({
       resultText: '',
       calculationText: '',
+      lastText: '',
     });
   }
 
   clearEntry() {
     this.setState({
       resultText: '',
+      lastText: '',
     });
   }
 
-  deleteLast() {
-    this.setState({});
+  deleteLast(text) {
+    this.setState({
+      resultText: this.state.resultText.slice(0, -1),
+      lastText: this.state.lastText.slice(0, -1),
+    });
   }
 
   calculate(text) {
+    this.setState({
+      calculationText: eval(this.state.resultText),
+    })
   }
+
 
   render() {
     return (
@@ -68,51 +108,51 @@ export default class App extends Component {
             </View>
             <View style={styles.row}>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('7')}
+                onPress={() => this.numberPressed('7')}
                 style={styles.icons2}>
                 <Text style={styles.font}>7</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('8')}
+                onPress={() => this.numberPressed('8')}
                 style={styles.icons2}>
                 <Text style={styles.font}>8</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('9')}
+                onPress={() => this.numberPressed('9')}
                 style={styles.icons2}>
                 <Text style={styles.font}>9</Text>
               </TouchableOpacity>
             </View>
             <View style={styles.row}>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('4')}
+                onPress={() => this.numberPressed('4')}
                 style={styles.icons2}>
                 <Text style={styles.font}>4</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('5')}
+                onPress={() => this.numberPressed('5')}
                 style={styles.icons2}>
                 <Text style={styles.font}>5</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('6')}
+                onPress={() => this.numberPressed('6')}
                 style={styles.icons2}>
                 <Text style={styles.font}>6</Text>
               </TouchableOpacity>
             </View>
             <View style={styles.row}>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('1')}
+                onPress={() => this.numberPressed('1')}
                 style={styles.icons2}>
                 <Text style={styles.font}>1</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('2')}
+                onPress={() => this.numberPressed('2')}
                 style={styles.icons2}>
                 <Text style={styles.font}>2</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('3')}
+                onPress={() => this.numberPressed('3')}
                 style={styles.icons2}>
                 <Text style={styles.font}>3</Text>
               </TouchableOpacity>
@@ -124,12 +164,12 @@ export default class App extends Component {
                 <Text style={styles.font}>+/-</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('0')}
+                onPress={() => this.numberPressed('0')}
                 style={styles.icons2}>
                 <Text style={styles.font}>0</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('.')}
+                onPress={() => this.dotPressed('.')}
                 style={styles.icons2}>
                 <Text style={styles.font}>.</Text>
               </TouchableOpacity>
@@ -138,26 +178,28 @@ export default class App extends Component {
           <View style={styles.operands}>
             <View style={styles.column}>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('/')}
+                onPress={() => this.operatorPressed('/')}
                 style={styles.icons}>
                 <Text style={styles.font}>/</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('*')}
+                onPress={() => this.operatorPressed('*')}
                 style={styles.icons}>
                 <Text style={styles.font}>*</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('-')}
+                onPress={() => this.operatorPressed('-')}
                 style={styles.icons}>
                 <Text style={styles.font}>-</Text>
               </TouchableOpacity>
               <TouchableOpacity
-                onPress={() => this.buttonsPressed('+')}
+                onPress={() => this.operatorPressed('+')}
                 style={styles.icons}>
                 <Text style={styles.font}>+</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.icons}>
+              <TouchableOpacity 
+                onPress={() => this.calculate()}
+                style={styles.icons}>
                 <Text style={styles.font}>=</Text>
               </TouchableOpacity>
             </View>
